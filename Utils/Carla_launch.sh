08a5eb35288d50fa32b8e710572833c151749c58
KJhari <harikrishnankj555@gmail.com>
2023-12-10 13:47:29 -0800

    added dark_net Training repo

--- Utils/Carla_launch.sh
+++ Utils/Carla_launch.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 #set the path to your simulator folder
-path=/opt/carla-simulator/
+path="/opt/carla-simulator"
 
 #set the quality as Low/Epic based
-quality=Low
+quality="Low"
 #set the simulation fps
 framesPerSecond=15
 
-
-cd $(path)
-export VK_ICD_FILENAMES="/usr/share/vulkan/icd.d/nvidia_icd.json"  && ./CarlaUE4.sh -fps=$(framesPerSecond) -quality-level=$(quality)
+# Navigate to the CARLA simulator directory
+cd "$path"
+# Set Vulkan ICD (Installable Client Driver) for NVIDIA
+export VK_ICD_FILENAMES="/usr/share/vulkan/icd.d/nvidia_icd.json"
+# Launch CARLA with the specified FPS and quality level
+./CarlaUE4.sh -fps=$framesPerSecond -quality-level=$quality
