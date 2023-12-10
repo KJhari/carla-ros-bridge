9cbee7e8ca2d0542133756cb8cde7816243b08f2
KJhari <harikrishnankj555@gmail.com>
2023-12-10 14:25:25 -0800

    update changeMap

--- Utils/changeMap.py
+++ Utils/changeMap.py
@@ -5,8 +5,8 @@ client = carla.Client('localhost', 2000)
 client.set_timeout(10.0) # seconds
 
 # Change the map
-map_name = 'Town07_Opt'  # Replace with the map you want to load
-#map_name = 'Town02_Opt'  # Replace with the map you want to load
+#map_name = 'Town07_Opt'  # Replace with the map you want to load
+map_name = 'Town02_Opt'  # Replace with the map you want to load
 
 world = client.load_world(map_name)
 
